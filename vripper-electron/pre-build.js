b9b20e86dd2552b30e3a147e0d392a068d8d3a33
death-claw <53543762+death-claw@users.noreply.github.com>
2019-10-01 23:27:01 +0100

    Strip libjvm.so for debian

--- vripper-electron/pre-build.js
+++ vripper-electron/pre-build.js
@@ -19,3 +19,7 @@ fs.writeFileSync('./build/vripper-ui/index.html', $.html());
 console.log('Building runtime environment');
 rimraf.sync('java-runtime');
 execSync('jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.base,java.desktop,java.instrument,java.management,java.naming,java.prefs,java.rmi,java.scripting,java.security.jgss,java.sql,jdk.httpserver,jdk.unsupported,jdk.crypto.ec --output java-runtime');
+if(process.platform === 'linux') {
+    console.log('Stripping libjvm.so');
+    execSync('strip -p --strip-unneeded java-runtime/lib/server/libjvm.so');
+}
