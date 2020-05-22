67c605e6405bf115e7c27e96930d60002db5ec9b
death-claw <53543762+death-claw@users.noreply.github.com>
2020-05-22 15:55:05 +0100

    v2.12.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [2.12.1] - 2020-05-22
+### Changed
+- Fix alternative name bug
+
 ## [2.12.0] - 2020-05-21
 ### Changed
 - Use new user hash from the api
--- vripper-ui/src/environments/environment.ts
+++ vripper-ui/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   localhost: 'http://localhost:8080',
   ws: 'ws://localhost:8080',
-  version: '2.12.0'
+  version: '2.12.1'
 };
 
 /*
