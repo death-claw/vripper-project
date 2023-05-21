a337fcfd4642b9fa22a466c7279bbe4ae6f987a7
death-claw <53543762+death-claw@users.noreply.github.com>
2023-05-21 15:44:16 +0100

    fixes #89

--- vripper-gui/src/main/kotlin/me/mnlr/vripper/view/settings/SettingsView.kt
+++ vripper-gui/src/main/kotlin/me/mnlr/vripper/view/settings/SettingsView.kt
@@ -18,25 +18,32 @@ class SettingsView : Fragment("Settings") {
     private val downloadSettingsView: DownloadSettingsView by inject()
     private val connectionSettingsView: ConnectionSettingsView by inject()
     private val viperSettingsView: ViperSettingsView by inject()
+    private val clipboardSettingsView: ClipboardSettingsView by inject()
 
     override val root = vbox(alignment = Pos.CENTER_RIGHT) {
         spacing = 5.0
         tabpane {
             tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
             VBox.setVgrow(this, Priority.ALWAYS)
-            tab<DownloadSettingsView>() {
+            tab<DownloadSettingsView> {
                 val imageView = ImageView("downloads-folder.png")
                 imageView.fitWidth = 18.0
                 imageView.fitHeight = 18.0
                 graphic = imageView
             }
-            tab<ConnectionSettingsView>() {
+            tab<ConnectionSettingsView> {
                 val imageView = ImageView("data-transfer.png")
                 imageView.fitWidth = 18.0
                 imageView.fitHeight = 18.0
                 graphic = imageView
             }
-            tab<ViperSettingsView>() {
+            tab<ClipboardSettingsView> {
+                val imageView = ImageView("clipboard.png")
+                imageView.fitWidth = 18.0
+                imageView.fitHeight = 18.0
+                graphic = imageView
+            }
+            tab<ViperSettingsView> {
                 val imageView = ImageView("icons/32x32.png")
                 imageView.fitWidth = 18.0
                 imageView.fitHeight = 18.0
@@ -56,7 +63,8 @@ class SettingsView : Fragment("Settings") {
                             settingsController.saveNewSettings(
                                 downloadSettingsView.downloadSettingsModel,
                                 connectionSettingsView.connectionSettingsModel,
-                                viperSettingsView.viperSettingsModel
+                                viperSettingsView.viperSettingsModel,
+                                clipboardSettingsView.clipboardSettingsModel
                             )
                             close()
                         } catch (e: ValidationException) {
