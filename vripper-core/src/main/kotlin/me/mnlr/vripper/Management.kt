70e7a87189bb55524ca9c61d46ce9e2ba9f05773
death-claw <53543762+death-claw@users.noreply.github.com>
2023-08-31 23:28:59 +0100

    dependencies update (#109)

--- vripper-core/src/main/kotlin/me/mnlr/vripper/Management.kt
+++ vripper-core/src/main/kotlin/me/mnlr/vripper/Management.kt
@@ -1,58 +1,19 @@
 package me.mnlr.vripper
 
 import jakarta.annotation.PreDestroy
-import org.springframework.beans.factory.annotation.Value
+import me.mnlr.vripper.download.DownloadService
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
-import me.mnlr.vripper.download.DownloadService
-import me.mnlr.vripper.services.AsyncTaskRunnerService
-import java.nio.file.Path
-import java.time.format.DateTimeFormatter
-import java.util.*
-import java.util.regex.Pattern
 
 @Component
 class Management(
     private val jdbcTemplate: JdbcTemplate,
-    @Value("\${base.dir}") baseDir: String,
-    @Value("\${base.dir.name}") baseDirName: String,
-    private val asyncTaskRunnerService: AsyncTaskRunnerService,
     private val downloadService: DownloadService
 ) {
-    private val backupFolder: Path
-
-    init {
-        backupFolder = Path.of(baseDir, baseDirName, "backup")
-    }
 
     @PreDestroy
     fun destroy() {
-//        taskRunnerService.destroy()
         downloadService.destroy()
         jdbcTemplate.execute("SHUTDOWN")
-    }
-
-//    @PostConstruct
-//    @Scheduled(cron = "0 0 0 ? * *")
-//    private fun backup() {
-//
-//        for (file in Files.list(backupFolder)) {
-//            val matcher = BACKUP_FILE_PATTERN.matcher(file.fileName.toString())
-//            if (matcher.find()) {
-//                val localDate = LocalDate.parse(matcher.group(1), FORMATTER)
-//                if (localDate.isEqual(LocalDate.now())) {
-//                    return
-//                }
-//            }
-//        }
-//
-//        // create a backup file
-//        val backupFile = backupFolder.resolve("db_${LocalDateTime.now().format(FORMATTER)}.tar.gz")
-//        jdbcTemplate.execute("BACKUP DATABASE TO '${backupFile}' BLOCKING")
-//    }
-
-    companion object {
-        val FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-        private val BACKUP_FILE_PATTERN = Pattern.compile("^db_(\\d{4}-\\d{2}-\\d{2})\\.tar\\.gz$")
     }
 }
